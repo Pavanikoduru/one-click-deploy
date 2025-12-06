2064d105ad563d4c6049544d2bed913a02d6fb3a
Pavani Koduru <116728929+Pavanikoduru@users.noreply.github.com>
2025-12-06 21:33:18 +0530

    Update userdata.sh

--- app/userdata.sh
+++ app/userdata.sh
@@ -4,7 +4,7 @@ yum install -y git
 curl -sL https://rpm.nodesource.com/setup_18.x | bash -
 yum install -y nodejs
 
-git clone https://github.com/<YOUR_GITHUB_USERNAME>/<YOUR_REPO>.git
+git clone https://github.com/Pavanikoduru/one-click-deploy.git
 cd <YOUR_REPO>/app
 npm install express
 nohup node server.js > app.log 2>&1 &

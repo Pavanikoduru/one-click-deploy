ef9d2a863630f05aa7c99519506229ef6252b99c
Pavani Koduru <116728929+Pavanikoduru@users.noreply.github.com>
2025-12-06 21:10:48 +0530

    Delete app/index.js

--- app/index.js
+++
@@ -1,6 +0,0 @@
-const http = require("http");
-const server = http.createServer((req, res) => {
-  if (req.url === "/health") return res.end("ok");
-  return res.end("Hello from EC2 behind ALB");
-});
-server.listen(8080);

f3d5775f8cac981796c161ce6efe3648659365d3
Pavani Koduru <116728929+Pavanikoduru@users.noreply.github.com>
2025-12-06 21:19:23 +0530

    Create userdata.sh

---
+++ app/userdata.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+yum update -y
+yum install -y git
+curl -sL https://rpm.nodesource.com/setup_18.x | bash -
+yum install -y nodejs
+
+git clone https://github.com/<YOUR_GITHUB_USERNAME>/<YOUR_REPO>.git
+cd <YOUR_REPO>/app
+npm install express
+nohup node server.js > app.log 2>&1 &

c88fea000000167c6a3b86a56c674ed98e153c4f
Pavani Koduru <116728929+Pavanikoduru@users.noreply.github.com>
2025-12-08 17:59:58 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,15 +1,19 @@
 provider "aws" {
-  region = "ap-south-1"
+  region = "ap-southeast-2"
 }
 
+# -----------------------------
+# VPC and Subnets
+# -----------------------------
 resource "aws_vpc" "vpc" {
   cidr_block = "10.0.0.0/16"
 }
 
 resource "aws_subnet" "public" {
-  count             = 2
-  vpc_id            = aws_vpc.vpc.id
-  cidr_block        = cidrsubnet(aws_vpc.vpc.cidr_block, 4, count.index)
+  count                   = 2
+  vpc_id                  = aws_vpc.vpc.id
+  availability_zone       = ["ap-southeast-2a", "ap-southeast-2b"][count.index]
+  cidr_block              = cidrsubnet(aws_vpc.vpc.cidr_block, 4, count.index)
   map_public_ip_on_launch = true
 }
 
@@ -19,17 +23,25 @@ resource "aws_subnet" "private" {
   cidr_block = cidrsubnet(aws_vpc.vpc.cidr_block, 4, count.index + 2)
 }
 
+# -----------------------------
+# Internet Gateway and NAT
+# -----------------------------
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.vpc.id
 }
 
-resource "aws_eip" "nat" {}
+resource "aws_eip" "nat" {
+  # Remove "vpc = true" to avoid Terraform error
+}
 
 resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.nat.id
   subnet_id     = aws_subnet.public[0].id
 }
 
+# -----------------------------
+# Route Tables
+# -----------------------------
 resource "aws_route_table" "public_rt" {
   vpc_id = aws_vpc.vpc.id
 }
@@ -46,6 +58,9 @@ resource "aws_route_table_association" "public_assoc" {
   route_table_id = aws_route_table.public_rt.id
 }
 
+# -----------------------------
+# Security Groups
+# -----------------------------
 resource "aws_security_group" "alb_sg" {
   vpc_id = aws_vpc.vpc.id
 
@@ -56,7 +71,12 @@ resource "aws_security_group" "alb_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  egress { from_port = 0 to_port = 0 protocol = "-1" cidr_blocks = ["0.0.0.0/0"] }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_security_group" "ec2_sg" {
@@ -69,37 +89,52 @@ resource "aws_security_group" "ec2_sg" {
     security_groups = [aws_security_group.alb_sg.id]
   }
 
-  egress { from_port = 0 to_port = 0 protocol = "-1" cidr_blocks = ["0.0.0.0/0"] }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
+# -----------------------------
+# Launch Template
+# -----------------------------
 resource "aws_launch_template" "lt" {
-  image_id      = "ami-0c2af51e265bd5e0e"
-  instance_type = "t2.micro"
+  name_prefix   = "asg-lt-"
+  image_id      = "ami-0b69ea66ff7391e80" # Amazon Linux 2 Free Tier
+  instance_type = "t2.micro"              # Free Tier eligible
+
   user_data = base64encode(<<EOF
 #!/bin/bash
 curl -sL https://rpm.nodesource.com/setup_18.x | bash -
 yum install -y nodejs git
-git clone https://github.com/<YOUR_GITHUB_USERNAME>/project.git
-cd project/app
+git clone https://github.com/Pavanikoduru/one-click-deploy.git
+cd one-click-deploy/app
 npm install
-npm start
+nohup node server.js > app.log 2>&1 &
 EOF
   )
+
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
 }
 
+# -----------------------------
+# Application Load Balancer
+# -----------------------------
 resource "aws_lb" "alb" {
   name               = "api-alb"
   load_balancer_type = "application"
-  subnets            = aws_subnet.public[*].id
+  subnets            = [for s in aws_subnet.public : s.id]
   security_groups    = [aws_security_group.alb_sg.id]
 }
 
 resource "aws_lb_target_group" "tg" {
-  name        = "api-tg"
-  port        = 8080
-  protocol    = "HTTP"
-  vpc_id      = aws_vpc.vpc.id
+  name     = "api-tg"
+  port     = 8080
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.vpc.id
+
   health_check {
     path = "/health"
   }
@@ -109,24 +144,41 @@ resource "aws_lb_listener" "listener" {
   load_balancer_arn = aws_lb.alb.arn
   port              = 80
   protocol          = "HTTP"
+
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.tg.arn
   }
 }
 
+# -----------------------------
+# Auto Scaling Group
+# -----------------------------
 resource "aws_autoscaling_group" "asg" {
-  desired_capacity     = 2
-  max_size             = 3
-  min_size             = 2
-  vpc_zone_identifier  = aws_subnet.private[*].id
+  desired_capacity    = 2
+  max_size            = 3
+  min_size            = 2
+  vpc_zone_identifier = [for s in aws_subnet.private : s.id]
+
   launch_template {
     id      = aws_launch_template.lt.id
     version = "$Latest"
   }
-  target_group_arns = [aws_lb_target_group.tg.arn]
+
+  target_group_arns          = [aws_lb_target_group.tg.arn]
+  health_check_type          = "ELB"
+  health_check_grace_period  = 60
+
+  tag {
+    key                 = "Name"
+    value               = "ASG-Instance"
+    propagate_at_launch = true
+  }
 }
 
+# -----------------------------
+# Output ALB DNS
+# -----------------------------
 output "alb_dns" {
   value = aws_lb.alb.dns_name
 }
